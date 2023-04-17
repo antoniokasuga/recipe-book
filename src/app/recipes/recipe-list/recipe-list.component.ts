85457e481288ff8cf231a8871e1fb20a8de025f6
a.polskihs <antons.polskihs@intrum.com>
2023-04-17 12:06:35 +0300

    Add chapter 10 related features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,25 +1,20 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tricolore-skillet-lasagna2-1650652993.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply another test', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/tricolore-skillet-lasagna2-1650652993.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private recipeService: RecipeService) {
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,45 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Skillet Lasagna',
+      'A mediocre lasagna',
+      'https://d33wubrfki0l68.cloudfront.net/89039f3824ad5d647fecf83c70b430d05d50e698/8c5a4/images/uploads/2021_12_20_skillet_lasagna_0.jpg',
+      [
+        new Ingredient('Lasagna noodles', 12),
+        new Ingredient('Onion', 1),
+        new Ingredient('Garlic Clove', 4),
+        new Ingredient('Sauce', 1)
+      ]),
+    new Recipe(
+      'Italian Pizza',
+      'A do-it-yourself pizza',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
+      [
+        new Ingredient('Frozen pizza base', 1),
+        new Ingredient('Tomato', 3),
+        new Ingredient('Cheese', 1),
+        new Ingredient('Sauce', 1)
+      ]),
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  };
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+
+}

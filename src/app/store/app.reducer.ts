77fc9ebf5faaca8ec73ea4820cd34376bf93ae68
a.polskihs <antons.polskihs@intrum.com>
2023-08-11 12:36:17 +0300

    Code clean up

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,5 +1,3 @@
-import { ActionReducerMap } from '@ngrx/store';
-
 import * as fromAuth from '../auth/store/auth.reducer';
 import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
 import * as fromRecipes from '../recipes/store/recipe.reducer';
@@ -9,9 +7,3 @@ export interface AppState {
   auth: fromAuth.State;
   recipes: fromRecipes.State
 }
-
-// export const appReducer: ActionReducerMap<AppState> = {
-//   shoppingList: fromShoppingList.shoppingListReducer,
-//   auth: fromAuth.authReducer,
-//   recipes: fromRecipes.recipeReducer
-// }
